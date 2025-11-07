5d60f8d721a7230ce98c096c38653a333e84dc6d
SatyamTiwari9336 <satyamtiwarisat@gmail.com>
2025-11-07 16:47:10 +0530

    done images both ways added

--- app/chai/lemon/page.tsx
+++ app/chai/lemon/page.tsx
@@ -1,3 +1,17 @@
+import Image from "next/image";
+
 export default function Lemonpage() {
-  return <div>lemon tea</div>;
+  const lemonimg: string = "/lemon.jpg";
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="text-6xl">Lemon tea</div>
+      <Image
+        className="mt-8"
+        src={lemonimg}
+        height={300}
+        width={300}
+        alt="Lemon tea"
+      />
+    </div>
+  );
 }
--- app/chai/page.tsx
+++ app/chai/page.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function ChaiPage() {
-  return <div className="text-center text-6xl mt-8">Chai Page</div>;
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="text-center text-6xl mt-8">Chai Page</div>
+
+      <img
+        className="mt-10"
+        src="/tea.jpg"
+        height={300}
+        width={300}
+        alt="Tea image "
+      />
+    </div>
+  );
 }
