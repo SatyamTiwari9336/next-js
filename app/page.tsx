f8f243073be116bb6f44599352a719bb7b01070f
SatyamTiwari9336 <satyamtiwarisat@gmail.com>
2025-11-07 16:07:43 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
       <h1 className=" bg-gradient-to-t from-black-500 to-gray-500  flex justify-center items-center m-3 p-10 text-7xl font-serif w-200 h-30 ">
-        Next js project
+        First Next js project
       </h1>
     </div>
   );
